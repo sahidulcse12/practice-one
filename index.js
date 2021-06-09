a1e0eed6b440e34d495004d38cb5c9c32853e620
sahidulcse12 <m.sahidulislam.14@gmail.com>
2021-06-09 08:17:55 +0600

    pushed

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = 3000;
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log('listening on port 3000')
+})
